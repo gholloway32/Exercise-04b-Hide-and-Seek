04b859c1ad0eae63a0a65770e37a0fbfab786709
gholloway32 <97569235+gholloway32@users.noreply.github.com>
2022-03-01 10:03:02 -0500

    assignment 4b done

--- Enemy/Enemy_Container.gd
+++ Enemy/Enemy_Container.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+onready var Bat = load("res://Enemy/Bat.tscn")
+onready var Turret = load("res://Enemy/Turret.tscn")
+
+func _physics_process(_delta):
+	if not has_node("Bat"):
+		var bat = Bat.instance()
+		add_child(bat)
+		bat.name = 'Bat'
+	if not has_node("Turret"):
+		var turret = Turret.instance()
+		add_child(turret)
+		turret.name = 'Turret'
